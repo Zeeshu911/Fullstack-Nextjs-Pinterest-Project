93cb5c4eed28bcae4a5e1167643b52e43a613f7c
ZeeshuCoder <codewithzeeshu122@gmail.com>
2024-11-01 15:06:19 +0500

    commit

---
+++ app/api/pin/[id]/route.js
@@ -0,0 +1,24 @@
+import connectToDB from "@/libs/mongodb";
+import Pin from "@/models/pin";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  try {
+    connectToDB();
+    const { id } = params;
+    const pin = await Pin.findById(id);
+    if (!pin) {
+      return NextResponse.json(
+        { success: false, message: "Pin not found." },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ success: true, pin }, { status: 200 });
+  } catch (error) {
+    console.error("Error while fetching pin.");
+    return NextResponse.json(
+      { success: false, error: "Error while fetching pin." },
+      { status: 500 }
+    );
+  }
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname:"res.cloudinary.com",
+                port: "",
+                pathname:"/didzyox00/**"
+            },
+            {
+                protocol: "https",
+                hostname:"lh3.googleusercontent.com",
+                port: "",
+                pathname:"/a/**"
+            },
+            {
+                protocol: "https",
+                hostname:"avatars.githubusercontent.com",
+                port: "",
+                pathname:"/u/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
